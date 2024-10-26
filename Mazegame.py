35cca2989dcfab441a1ae942ce10e158abc7342d
hse <c01039240448@icloud.com>
2024-10-26 15:55:40 +0900

    jj

--- Mazegame.py
+++ Mazegame.py
@@ -4,15 +4,15 @@ from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 
 app = Ursina()
 
-player = PlatformerController2d()
+
 
 camera.orthographic = True #카메라를 직교로 설치
 camera.fov = 20 #카메라가 수직으로 볼 수 있는 단위 수를 설정 fov(field of view = 시야각)
 
 ground = Entity(
     model='cube',
-    color=color.rgb(50, 180, 50),
-    z=0,
+    color=color.green,
+    z=0.1,
     y=-8,
     scale=(100, 5, 10),
     collider='box'
@@ -20,9 +20,19 @@ ground = Entity(
 
 player = PlatformerController2d(
     position=(-15, -5),
+    texture='monster_4925777.png',
     color=color.white,
-    scale=1,
-    max_jumps=2
+    scale=1.5,
+    max_jumps=1
 )
+
+background = Entity(
+    model='cube',
+    texture='background.png',
+    scale=(40, 20, 1),
+    z=2
+)
+
+
 
 app.run()
