064d144a6511fd4311fedc62dc2c563e11021535
sieun-han <c01039240448@icloud.com>
2024-11-09 15:38:14 +0900

    Update jangamulGame.py

--- jangamulGame.py
+++ jangamulGame.py
@@ -4,59 +4,115 @@ from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 
 app = Ursina()
 
+import random
 
-
-camera.orthographic = True #카메라를 직교로 설치
-camera.fov = 20 #카메라가 수직으로 볼 수 있는 단위 수를 설정 fov(field of view = 시야각)
+camera.orthographic = True
+camera.fov = 40
+#카메라를 직교로 설치
+#카메라가 수직으로 볼 수 있는 단위 수를 설정 fov(field of view = 시야각)
 
 ground = Entity(
     model='cube',
     color=color.green,
     z=0.1,
     y=-8,
-    scale=(100, 5, 10),
+    scale=(100, 15, 10),
     collider='box'
 )
 
 player = PlatformerController2d(
-    position=(-15, -5),
+    position=(-30, 0),
     texture='monster_4925777.png',
     color=color.white,
-    scale=1.5,
-    max_jumps=1
+    scale=(1.5),
+    max_jumps=2,
+    collider='box'
 )
 
 background = Entity(
     model='cube',
     texture='background.png',
-    scale=(40, 20, 1),
+    scale=(80, 40, 1),
     z=2
 )
 
+obstacles = []
 for i in range(10):
     obstacle = Entity(
-        position=(random.randint(-10, 10), -5),
+        position=(random.randint(-25, 30), 0),
         model='cube',
-        color=color.white,
-        scale=1
+        color=color.red,
+        texture='spike (3).png',
+        scale=1.7,
+        collider='sphere'
     )
+    obstacles.append(obstacle)
+
+wall= Entity(
+    model='cube',
+    collider='box',
+    color=color.green,
+    position=(20,6),
+    scale=(5,1)
+)
+
+wall2= Entity(
+    model='cube',
+    collider='box',
+    color=color.green,
+    position=(-20,6),
+    scale=(5,1)
+)
+
+wall3= Entity(
+    model='cube',
+    collider='box',
+    color=color.green,
+    position=(0,8),
+    scale=(3,1)
+)
+
+wall4= Entity(
+    model='cube',
+    collider='box',
+    color=color.green,
+    position=(-10,7),
+    scale=(5,1)
+)
+
+wall5= Entity(
+    model='cube',
+    collider='box',
+    color=color.green,
+    position=(10,7),
+    scale=(5,1)
+)
 
 class Exit(Entity):
     def __init__(self, x, z):
         super().__init__(
             model='cube',
-            color=color.green,
-            scale=(0.1, 0.1, 0.1),
-            position=(x, 0, z)
-            )
-self.player = player #외부 플레이어의 정보를 Exit 클래스의 self.player 변수에 저장
+            color=color.black,
+            scale=(1, 0.5, 1),
+            position=(x, -0.5, z),
+            collider = 'box'
+        )
 
-def update(self):
-    self.playerCollision()
+exit_entity = Exit(33, 0)
 
-def update(self):
-        if player.intersects(self).hit:
-            print("게임 오버! 장애물에 닿았습니다.")
-            Text(text="게임 오버", origin=(0, 0), scale=2)
-            invoke(application.quit, delay=2)  # 2초 뒤에 게임 종료
+def game_over():
+     print("도착지점에 도달했습니다. 게임 종료!")
+     application.quit()
+
+def reset_player():
+    player.position = (-30, 0)
+
+def update():
+   for obstacle in obstacles:
+        if player.intersects(obstacle).hit:
+            print("장애물에 닿았습니다! 시작지점으로 돌아갑니다.")
+            reset_player()
+   if player.intersects(exit_entity).hit:
+      game_over()
+
 app.run()
